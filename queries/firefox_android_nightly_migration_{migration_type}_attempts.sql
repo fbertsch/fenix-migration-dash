489b5a6d9812f7338fbd658c16af212000c664b3
Frank Bertsch <fbertsch@mozilla.com>
2020-01-21 10:53:28 -0500

    Fix attempts to use correct date

--- queries/firefox_android_nightly_migration_{migration_type}_attempts.sql
+++ queries/firefox_android_nightly_migration_{migration_type}_attempts.sql
@@ -9,8 +9,8 @@ WITH  client_attempts AS (
   FROM
     `moz-fx-data-shared-prod.analysis.org_mozilla_fennec_aurora_migration`
   WHERE
-    DATE(submission_timestamp) > {start_date}
-    AND DATE(submission_timestamp) < {end_date}
+    DATE(submission_timestamp) >= {start_date}
+    AND DATE(submission_timestamp) <= {end_date}
     AND udf.get_key(metrics.labeled_string.migration_migration_versions, '{migration_type}') IS NOT NULL
 ), counts AS (
   SELECT
