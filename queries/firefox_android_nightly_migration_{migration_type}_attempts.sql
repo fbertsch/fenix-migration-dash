481288420013460bb1530c07be3cd5525a725378
Frank Bertsch <fbertsch@mozilla.com>
2020-01-21 11:53:29 -0500

    Fix attempts for non-version reporting migrations

--- queries/firefox_android_nightly_migration_{migration_type}_attempts.sql
+++ queries/firefox_android_nightly_migration_{migration_type}_attempts.sql
@@ -5,13 +5,16 @@ WITH  client_attempts AS (
     TIMESTAMP_TRUNC(submission_timestamp, MINUTE) AS minute,
     ROW_NUMBER() OVER (PARTITION BY client_info.client_id ORDER BY submission_timestamp ASC) AS attempt_number,
     metrics.boolean.migration_{migration_type}_any_failures AS any_failures,
-    udf.get_key(metrics.labeled_string.migration_migration_versions, '{migration_type}') AS migration_version
+    coalesce(udf.get_key(metrics.labeled_string.migration_migration_versions, '{migration_type}'), '1') AS migration_version
   FROM
     `moz-fx-data-shared-prod.analysis.org_mozilla_fennec_aurora_migration`
   WHERE
     DATE(submission_timestamp) >= {start_date}
     AND DATE(submission_timestamp) <= {end_date}
-    AND udf.get_key(metrics.labeled_string.migration_migration_versions, '{migration_type}') IS NOT NULL
+    AND (metrics.boolean.migration_{migration_type}_any_failures IS NOT NULL
+         OR ('{migration_type}' = 'settings' AND metrics.counter.migration_settings_success_reason IS NOT NULL)
+         OR ('{migration_type}' = 'addons' AND udf.get_key(metrics.labeled_string.migration_migration_versions, '__other__') IS NOT NULL)
+         OR udf.get_key(metrics.labeled_string.migration_migration_versions, '{migration_type}') IS NOT NULL)
 ), counts AS (
   SELECT
     minute,
