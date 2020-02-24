502a6e26a32b7578cd645782562fb84c4af3b8a7
Frank Bertsch <fbertsch@mozilla.com>
2020-02-24 12:04:09 -0500

    Fix total client migration query

--- queries/firefox_android_nightly_migration_total_clients.sql
+++ queries/firefox_android_nightly_migration_total_clients.sql
@@ -1,38 +1,6 @@
-WITH pings AS (
+WITH migrated_client_pings AS (
   SELECT
-    DATE(submission_timestamp) AS date,
-    submission_timestamp,
-    document_type,
-    `moz-fx-data-shared-prod.udf_js.gunzip`(payload) AS payload,
-  FROM
-    `moz-fx-data-shared-prod.payload_bytes_error.structured`
-  WHERE
-    DATE(submission_timestamp) >= {start_date}
-    AND DATE(submission_timestamp) <= {end_date}
-    AND document_namespace = "org-mozilla-fennec-aurora"
-    AND (document_type = "baseline" OR document_type = "migration")
-), migrated_client_pings AS (
-  SELECT
-    TIMESTAMP_TRUNC(submission_timestamp, MINUTE) AS minute,
-    JSON_EXTRACT(payload, "$.client_info.client_id") AS client_id
-  FROM
-    pings
-  WHERE
-    document_type = "baseline"
-    AND DATE(submission_timestamp) <= '2020-01-20'
-  UNION ALL
-  SELECT
-    TIMESTAMP_TRUNC(submission_timestamp, MINUTE) AS minute,
-    JSON_EXTRACT(payload, "$.client_info.client_id") AS client_id
-  FROM
-    pings
-  WHERE
-    document_type = "migration"
-    AND STRPOS(payload, "telemetry_identifiers") > 0
-    AND DATE(submission_timestamp) <= '2020-01-20'
-  UNION ALL
-  SELECT
-    TIMESTAMP_TRUNC(submission_timestamp, MINUTE) AS minute,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS hour,
     client_info.client_id AS client_id
   FROM
     `moz-fx-data-shared-prod.org_mozilla_fennec_aurora_stable.migration_v1`
@@ -41,23 +9,40 @@ WITH pings AS (
     AND DATE(submission_timestamp) < current_date
   UNION ALL
   SELECT
-    TIMESTAMP_TRUNC(submission_timestamp, MINUTE) AS minute,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS hour,
     client_info.client_id AS client_id
   FROM
     `moz-fx-data-shared-prod.org_mozilla_fennec_aurora_live.migration_v1`
   WHERE
     DATE(submission_timestamp) = current_date
+  UNION ALL
+  SELECT
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS hour,
+    client_info.client_id AS client_id
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_fennec_aurora_stable.baseline_v1`
+  WHERE
+    DATE(submission_timestamp) >= '2020-01-20'
+    AND DATE(submission_timestamp) < current_date
+  UNION ALL
+  SELECT
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS hour,
+    client_info.client_id AS client_id
+  FROM
+    `moz-fx-data-shared-prod.org_mozilla_fennec_aurora_live.baseline_v1`
+  WHERE
+    DATE(submission_timestamp) = current_date
 ), migrated_clients AS (
   SELECT
-    minute,
+    hour,
     COUNT(DISTINCT client_id) AS client_count
   FROM
     migrated_client_pings
   GROUP BY
-    minute
+    hour
 ), unmigrated_clients AS (
   SELECT
-    TIMESTAMP_TRUNC(submission_timestamp, MINUTE) AS minute,
+    TIMESTAMP_TRUNC(submission_timestamp, HOUR) AS hour,
     COUNT(DISTINCT client_id) AS client_count
   FROM
     `moz-fx-data-shared-prod.{dataset}.core{core_version_suffix}`
@@ -68,10 +53,10 @@ WITH pings AS (
     AND normalized_os = "Android"
     AND metadata.uri.app_update_channel = "nightly"
   GROUP BY
-    minute
+    hour
 ), all_clients AS (
   SELECT
-    minute,
+    hour,
     COALESCE(a.client_count, 0) AS migrated_client_count,
     COALESCE(b.client_count, 0) AS unmigrated_client_count,
     COALESCE(a.client_count, 0) + COALESCE(b.client_count, 0) AS client_count
@@ -79,11 +64,11 @@ WITH pings AS (
     migrated_clients a
   FULL OUTER JOIN
     unmigrated_clients b
-    USING (minute)
+    USING (hour)
 )
 
 SELECT
-  DATE(minute) AS date,
+  DATE(hour) AS date,
   *
 FROM
   all_clients
